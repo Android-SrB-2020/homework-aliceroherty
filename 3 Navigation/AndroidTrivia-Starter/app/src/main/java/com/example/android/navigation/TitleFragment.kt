446c945c2d06854322c2e8e890cc173b2ebed156
Alice Roherty-Carrier <aliceroherty@gmail.com>
2020-01-27 13:09:38 -0400

    Started 3.2

--- 3 Navigation/AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ 3 Navigation/AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -2,12 +2,12 @@ package com.example.android.navigation
 
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 /**
@@ -15,11 +15,34 @@ import com.example.android.navigation.databinding.FragmentTitleBinding
  */
 class TitleFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater, R.layout.fragment_title, container, false)
+
+        //Play Button Click Listener
+        binding.playButton.setOnClickListener {
+            //Navigate to the Game Fragment
+            it.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+        }
+
+        //Enabling Options Menu
+        setHasOptionsMenu(true)
+
         return binding.root
     }
 
+    /**
+     * If the option doesn't navigate where you want it to, make sure the options ID is the same as the fragments ID
+     */
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+        //Inflating Options Menu
+        inflater.inflate(R.menu.options_menu, menu)
+    }
+
+    //Onclick for Option
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, view!!.findNavController()) || super.onOptionsItemSelected(item)
+    }
 }
